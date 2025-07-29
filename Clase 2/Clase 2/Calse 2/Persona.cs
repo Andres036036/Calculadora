66fa87a7d7215aec871d8c2b8defbd1a5611f80a
Andres036036 <didjudke@gmail.com>
2025-07-29 09:51:33 -0500

    Add files via upload

---
+++ Clase 2/Clase 2/Calse 2/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calse_2
+{
+    internal class Persona
+    {
+        private int _id;
+        private string _name;
+
+        public Persona(int id, string name) 
+        { 
+            _id = id;
+            _name = name;
+        }
+    }
+}
